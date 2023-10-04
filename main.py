d3b7175b7f9f3f4428074192fa564cabfe6d27d3
ahrandeep <675612+ahrandeep@users.noreply.github.com>
2023-10-04 15:40:51 +0100

    Testing staged commits. Removed comments from main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-########################
-#        IMPORTS       #
-########################
 import functions.sheets_api as sheets_api
 from functions.generate_grid import generate_grid
 from functions.populate_grid import populate_grid
 from functions.print_grid import print_grid
 
 
-
-########################
-#      MAIN RUNTIME    #
-########################
 def main():
     month, year = 10, 2023
     grid = generate_grid(month, year)
 
-    grid = populate_grid(grid, 5)
+    grid = populate_grid(grid, 7)
 
     print_grid(grid)
-    
+
     sheets_api.write_data(grid)
+
 
 if __name__ == '__main__':
     main()
