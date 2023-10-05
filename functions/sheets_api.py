d168d0c694607f72b77f72172399721f83b5ab37
ahrandeep <675612+ahrandeep@users.noreply.github.com>
2023-10-05 13:37:20 +0100

    - Added format changes for generated sheet

--- functions/sheets_api.py
+++ functions/sheets_api.py
@@ -70,25 +70,49 @@ def write_data(grid: Grid) -> bool:
         service = build('sheets', 'v4', credentials=creds)
 
         sheetName = grid.dates[0].strftime("Rota - %b %y")
-        dup = {
-            "sourceSheetId": 0,
-            "insertSheetIndex": 1,
-            "newSheetName": sheetName
-        }
-        req = {
-            "requests": [{
-                "duplicateSheet": dup,
-            }]
-        }
         sheet = service.spreadsheets()
-        sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=req).execute()
+        sheetId = None
 
+        sheetProps = sheet.get(spreadsheetId=SPREADSHEET_ID,
+                        fields = ("sheets.properties.title,"
+                                    "sheets.properties.sheetId")
+                    ).execute()
 
+        # checks sheet titles to see if sheet already exists
+        sheetId = next((ele["properties"]["sheetId"] 
+                            for ele
+                            in sheetProps["sheets"]
+                            if (ele["properties"]["title"] == sheetName)),
+                            None)
+
+        if sheetId is None:
+            dup = {
+                "sourceSheetId": 0,
+                "insertSheetIndex": 1,
+                "newSheetName": sheetName
+            }
+            req = {
+                "requests": [{
+                    "duplicateSheet": dup,
+                }]
+            }
+            # Duplicates base rota sheet with name as new sheetName
+            newSheet = sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=req).execute()
+            sheetId = newSheet["replies"][0]["duplicateSheet"]["properties"]["sheetId"]
+            print(f"New Sheet generated - '{sheetName}'")
+        else:
+            print(f"Sheet already exists - {sheetName}")
     except HttpError as err:
         print(err)
-
+        return None
     try:
+        # generate values for sheet input. Each row is a new list inside values
+        # values = [
+        #   [cell_data,...],
+        #   ...
+        # ]
         values = []
+        rowsToFormat = False
 
         row_count = 0
         for count, date in enumerate(grid.dates):
@@ -98,16 +122,94 @@ def write_data(grid: Grid) -> bool:
                     code = event.assigned.code if event.assigned is not None else "None"
                     values[count].append(f'{code}')
                 row_count += 1
-
+        
+        # Fill in values up to 31 days (removes any dates which aren't needed)
+        valueLen = len(values)
+        if valueLen < 31:
+            rowsToFormat = True
+            for i in range(valueLen, 31):
+                values.append([""])
+    
         body = {
             "values": values
         }
-
+        
         sheet.values().update(spreadsheetId=SPREADSHEET_ID,
                               range=f"{sheetName}!A2:G32",
                               valueInputOption="USER_ENTERED",
                               body=body).execute()
-    
+        print(f"Values updated for sheet - '{sheetName}'")
+
+        # Format cells with borders
+        # and remove data validation for months with less than 31 days
+        if rowsToFormat:
+            colorStyle = {
+                "rgbColor": {
+                    "red": 0,
+                    "green": 0,
+                    "blue": 0,
+                    "alpha": 1
+                }
+            }
+            noBorderStyle = {
+                "style": "NONE",
+                "colorStyle": colorStyle
+            }
+            solidBorderStyle = {
+                "style": "SOLID",
+                "colorStyle": colorStyle
+            }
+            update = {
+                "requests": [{
+                    "updateBorders": {
+                        "range": {
+                            "sheetId": sheetId,
+                            "startRowIndex": 0,
+                            "endRowIndex": 33,
+                            "startColumnIndex": 0,
+                            "endColumnIndex": 9
+                        },
+                        "top": noBorderStyle,
+                        "bottom": noBorderStyle,
+                        "left": noBorderStyle,
+                        "right": noBorderStyle,
+                        "innerHorizontal": noBorderStyle,
+                        "innerVertical": noBorderStyle
+                    }
+                },
+                {
+                    "updateBorders": {
+                        "range": {
+                            "sheetId": sheetId,
+                            "startRowIndex": 0,
+                            "endRowIndex": valueLen + 1,
+                            "startColumnIndex": 0,
+                            "endColumnIndex": 9
+                        },
+                        "top": solidBorderStyle,
+                        "bottom": solidBorderStyle,
+                        "left": solidBorderStyle,
+                        "right": solidBorderStyle,
+                        "innerHorizontal": solidBorderStyle,
+                        "innerVertical": solidBorderStyle
+                    },
+                },
+                {
+                    "setDataValidation": {
+                        "range": {
+                            "sheetId": sheetId,
+                            "startRowIndex": valueLen + 1,
+                            "endRowIndex": 32,
+                            "startColumnIndex": 1,
+                            "endColumnIndex": 9
+                        }
+                    }
+                }]
+            }
+            
+            sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=update).execute()
+            print(f"Format updated for sheet - '{sheetName}'")
+
     except HttpError as err:
         print(err)
         return None
