e054da701a93633a152c206f81f1245b3b402107
ahrandeep <675612+ahrandeep@users.noreply.github.com>
2023-10-06 12:48:39 +0100

    Updated populate_grid -- Previously populated in date order (0->31) --> Now picks random rows until all rows filled Removed seed for filling first event -- No longer needed to add randomness as random date is picked

--- functions/populate_grid.py
+++ functions/populate_grid.py
@@ -1,35 +1,40 @@
-import random
+from random import randint
 
 from classes.grid import Grid
 from functions.parse_event import parse_event
-from functions.check_event import check_event
 
 
-def populate_grid(grid: Grid, seed: int, verbose: bool = False) -> None:
+def populate_grid(grid: Grid, verbose: bool = False) -> None:
     """Fills in grid by assigning users to events
     
     Loops through rows either forwards or backwards randomly
 
     Args:
         grid: The main grid to populate
-        seed: An int used to determine starting assignment in the first event
         verbose: A bool indicating whether to print verbose statements 
     """
 
     num_users = len(grid.users) - 1
+    num_rows = len(grid.events) - 1
+    populated_rows = []
 
-    # Assigned first event with seed
-    grid.events[0][0].assign(check_event(grid, grid.events[0][0], grid.events[0], seed))
-    # Loop through rows
-    for row in grid.events:
-      # Loop through columns
-      if random.randint(0, 1) == 0:
+    while len(populated_rows) <= num_rows:
+        row_to_pop = randint(0, num_rows)
+
+        if row_to_pop in populated_rows:
+            continue
+
+        row = grid.events[row_to_pop]
+
+        if randint(0, 1) == 0:
           for event in row:
               parse_event(grid, event, row, num_users, verbose)
-      else:
-          col_num = len(row) - 1
-          while col_num >= 0:
-              parse_event(grid, row[col_num], row, num_users, verbose)
-              col_num -= 1
-          
+        else:
+            col_num = len(row) - 1
+            while col_num >= 0:
+                parse_event(grid, row[col_num], row, num_users, verbose)
+                col_num -= 1
+
+        populated_rows.append(row_to_pop)
+
     return grid
